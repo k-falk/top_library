e4b145255f6b2533bc4259b5b5b2c744491f7875
k-falk <xkfalkx@gmail.com>
2022-02-02 22:18:43 -0500

    Add button for adding books Add popup for entering book info Add button for toggling is read

--- script.js
+++ script.js
@@ -26,22 +26,6 @@ function Book(title, author, pagesRead, isRead){
     }
 
 
-function addBookToLibrary(title, author, pagesRead, isRead) {
-
-    const newBook = new Book(title, author, pagesRead, isRead);
-    myLibrary.push(newBook);
-}
-
-function displayBooks(){
-    const library = document.querySelector(".library")
-
-    myLibrary.forEach((book) => {
-        const bookDisplay = addBookContent(book);
-        bookDisplay.classList.add("book");
-        library.appendChild(bookDisplay);
-    });
-}
-
 function addBookContent(book){
     const bookContent = document.createElement("div");
     const title = document.createElement("h3");
@@ -56,14 +40,73 @@ function addBookContent(book){
     pagesRead.innerText = book.pagesRead;
     bookContent.appendChild(pagesRead);
 
+    const isRead = document.createElement("h3");
+    isRead.innerText = book.isRead ? "Read":"Not Read";
+    bookContent.appendChild(isRead);
+
 
     return bookContent;
 }
 
+const addBookButton = document.querySelector(".add-book-button");
+addBookButton.addEventListener("click", () => {
+    const modal = document.querySelector("#modal");
+    modal.classList= "open"
+
+});
 
 
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien",295, false);
-addBookToLibrary("Cool Beans", "J.R.R. Tolkien",295, false);
-addBookToLibrary("Cool Runnings", "J.R.R. Tolkien",295, false);
+const getBookFromInput = () => {
+    const title = document.getElementById('title').value
+    document.getElementById('title').value = "";
+    const author = document.getElementById('author').value
+    document.getElementById('author').value = "";
+    const pages = document.getElementById('pages').value
+    document.getElementById('pages').value = "";
+    const isRead = document.getElementById('isRead').checked
+    document.getElementById('isRead').checked = false;
+    return new Book(title, author, pages, isRead)
+}
 
-displayBooks();
+
+const addBookForm = document.getElementById("addBookForm");
+addBookForm.onsubmit = addBookToLibrary;
+
+
+function addBookToLibrary(e) {
+    e.preventDefault();
+    const modal = document.querySelector("#modal");
+    modal.classList = "close"
+
+    const newBook = getBookFromInput();
+    myLibrary.push(newBook);
+    displayBooks();
+}
+
+
+
+function displayBooks(){
+    const library = document.querySelector(".library")
+
+    while (library.firstChild) {
+        library.removeChild(library.firstChild);
+    }
+    myLibrary.forEach((book) => {
+    const bookDisplay = addBookContent(book);
+    bookDisplay.classList.add("book");
+    library.appendChild(bookDisplay);
+
+    });
+}
+
+
+
+
+
+// TODO
+// Add button for adding books
+// Add popup for entering book info
+// Add button for toggling is read
+
+// Tweak book display
+// Add button for removing
